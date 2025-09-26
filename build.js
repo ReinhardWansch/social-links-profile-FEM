1e273a84f35205db2681727cc188f515ab1a9dff
Reinhard Wansch <reinhard@wansch.at>
2025-09-26 19:20:32 +0200

    add scss color varibles, set page-background

--- build.js
+++ build.js
@@ -5,8 +5,11 @@ esbuild.build({
     entryPoints: ['src/scss/index.scss'],
     bundle: true,
     outfile: 'dist/css/style.css',
+    sourcemap: true,
     plugins: [sassPlugin({quietDeps: true})]
 }).catch((e) => {
     // console.error('Build failed:', e.message);
     process.exit(1);
+}).finally(() => {
+    console.log('Build finished');
 });
